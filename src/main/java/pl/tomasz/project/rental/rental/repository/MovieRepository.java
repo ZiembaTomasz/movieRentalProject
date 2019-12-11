271ccc2de445e81364f663faea7dda59b9f5902e
ziembatomasz <ziembatomasz@gmail.com>
2019-12-11 03:19:49 +0100

    newFeatures

--- src/main/java/pl/tomasz/project/rental/rental/repository/MovieRepository.java
+++ src/main/java/pl/tomasz/project/rental/rental/repository/MovieRepository.java
@@ -3,5 +3,8 @@ package pl.tomasz.project.rental.rental.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.tomasz.project.rental.rental.domain.Movie;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByNameLike(String name);
 }
